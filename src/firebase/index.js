c99abd7045267ad3b92a43189a1a8313a03a43c7
Jonathan Zhou <jonnyzgg@gmail.com>
2024-12-10 11:31:00 -0500

    added firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDXr05BrgjmDLaWV3nz77v-ovmJOMnhV7M",
+    authDomain: "summative-a9445.firebaseapp.com",
+    projectId: "summative-a9445",
+    storageBucket: "summative-a9445.firebasestorage.app",
+    messagingSenderId: "1057816891558",
+    appId: "1:1057816891558:web:002c68729d3c006881a467"
+};
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
